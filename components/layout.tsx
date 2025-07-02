ad1817a4f221a38d576f3f48ffcc490516facdaf
Luciogi <84625092+Luciogi@users.noreply.github.com>
2025-07-02 13:07:36 +0500

    ui/layout: add container to center page on large screen

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,15 @@
 import { Nav } from "./navbar";
-import  Foot  from "./footer";
+import Foot from "./footer";
 
-export default function Layout({children}) {
-    return (
-        <>
-            <Nav/>
-                <main>
-                    { children } 
-                </main>
-            <Foot/>
-        </>
-    );
+export default function Layout({ children }) {
+  return (
+    <>
+      <main class="md:container md:mx-auto">
+        <Nav />
+        <main>{children}</main>
+        <Foot />
+      </main>
+    </>
+  );
 }
+
